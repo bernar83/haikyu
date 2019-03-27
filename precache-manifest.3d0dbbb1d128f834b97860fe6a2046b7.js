c1f08f845f201938d80f010baaf879beb1bc94c9
Adrian Bernardo <nabernardo93@gmail.com>
2019-03-26 22:26:41 -0700

    Updates

---
+++ precache-manifest.3d0dbbb1d128f834b97860fe6a2046b7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "6fd30de603cf4e1e744b",
+    "url": "/haikyu/static/js/runtime~main.6ce6bc0b.js"
+  },
+  {
+    "revision": "783b1c1b51d41f36f5fb",
+    "url": "/haikyu/static/js/main.23222d92.chunk.js"
+  },
+  {
+    "revision": "13a9fe37070b8b5161c4",
+    "url": "/haikyu/static/js/2.923a7c21.chunk.js"
+  },
+  {
+    "revision": "68837a8a58c63707e2e7dc5b84dab993",
+    "url": "/haikyu/index.html"
+  }
+];
